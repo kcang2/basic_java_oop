339fac88f5392168779d4dbd363c46ee54a15550
kcang2 <42316301+kcang2@users.noreply.github.com>
2020-01-14 18:52:41 +0800

    Update SortedList.java

--- SortedList.java
+++ SortedList.java
@@ -7,6 +7,14 @@ class SortedList<T extends Comparable<T>> {  // Comparable is crucial
     list = new LinkedList<>(Arrays.asList(input));
     Collections.sort(list);
   }
+  public void merge(SortedList<T> list2) {
+    list.addAll(list2.getList());
+  }
+
+  public LinkedList<T> getList() {
+    return list;
+  } 
+
   public String toString() {
     return list.toString();
   }
@@ -19,6 +27,10 @@ class Main {
     for (int i = 0; i< 25; ++i)
       input[i] = rand.nextInt(101);
     SortedList<Integer> sl = new SortedList<Integer>(input);
+    Integer[] input2 = {999, 1000};
+    SortedList<Integer> sl2 = new SortedList<Integer>(input2);
+    System.out.println(sl2);
+    sl.merge(sl2);
     System.out.println(sl);
   }
 }
